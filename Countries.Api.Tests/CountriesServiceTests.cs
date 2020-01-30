90ed7e1118d2a761a180ff5cb23e35872c87643d
Borche Ivanov <borche.ivanov@polarcape.com>
2020-01-30 10:06:00 +0100

    FakeItEasy added to the project

---
+++ Countries.Api.Tests/CountriesServiceTests.cs
@@ -0,0 +1,25 @@
+﻿
+using Countries.Api.Services;
+using FakeItEasy;
+using Xunit;
+using FluentAssertions;
+
+
+namespace Countries.Api.Tests
+{
+    public class CountriesServiceTests
+    {
+        [Fact]
+        public async void TestOne()
+        {
+            var countriesCollection = A.CollectionOfFake<Models.Country>(25);
+            var service = A.Fake<ICountriesService>();
+            var countries = new Models.Countries();
+            countries.AddRange(countriesCollection);
+            A.CallTo( () => service.GetAllAsync()).Returns(countries);
+
+            var result = await service.GetAllAsync();
+            result.Should().BeOfType<Models.Countries>();
+        }
+    }
+}
