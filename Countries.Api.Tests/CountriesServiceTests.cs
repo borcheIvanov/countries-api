48c18f627e54ee9adf61d4f627a13996601b3128
Borche Ivanov <borche.ivanov@polarcape.com>
2020-02-07 15:59:11 +0100

    Update CountriesServiceTests.cs

--- Countries.Api.Tests/CountriesServiceTests.cs
+++ Countries.Api.Tests/CountriesServiceTests.cs
@@ -1,25 +1,71 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Countries.Api.Models;
+using Countries.Api.Repositories;
 using Countries.Api.Services;
 using FakeItEasy;
-using Xunit;
 using FluentAssertions;
-
+using Xunit;
 
 namespace Countries.Api.Tests
 {
     public class CountriesServiceTests
     {
-        [Fact]
-        public async void TestOne()
+        private readonly ICountriesRepository _repository;
+        private readonly ICountriesService _service;
+        public CountriesServiceTests()
         {
-            var countriesCollection = A.CollectionOfFake<Models.Country>(25);
-            var service = A.Fake<ICountriesService>();
-            var countries = new Models.Countries();
-            countries.AddRange(countriesCollection);
-            A.CallTo( () => service.GetAllAsync()).Returns(countries);
-
-            var result = await service.GetAllAsync();
-            result.Should().BeOfType<Models.Countries>();
+            _repository = A.Fake<ICountriesRepository>();
+            _service = new CountriesService(_repository);
         }
+        
+        [Fact]
+        public async void GetAllAsyncShouldBeNotNull()
+        {
+            A.CallTo(() => _repository.GetAllAsync()).Returns(new List<Country>());
+
+            var result = await _service.GetAllAsync();
+            result.Should().NotBeNull();
+        }
+        
+        [Fact]
+        public async void GetAllAsyncShouldReturnListOfCountries()
+        {
+            var countries = A.CollectionOfFake<Country>(25);
+         
+            A.CallTo(() => _repository.GetAllAsync()).Returns(countries);
+
+            var result = await _service.GetAllAsync();
+            result.Should().BeOfType<List<Country>>();
+        }
+        
+        [Fact]
+        public async void GetAllAsyncShouldReturnAllCountries()
+        {
+            var countries = A.CollectionOfDummy<Country>(25);
+            A.CallTo(() => _repository.GetAllAsync()).Returns(countries);
+
+            var result = await _service.GetAllAsync();
+            result.Should().HaveCount(25);
+        }
+        
+        [Fact]
+        public async void FakERepository()
+        {
+            var countries = A.CollectionOfDummy<Country>(25);
+            var country = new Country {Alpha3Code = "MKD", Name = "Macedonia", Capital = "Skopje"};
+            countries.Add(country);
+            
+            A.CallTo(() => _repository.GetByCodeAsync("MKD")).Returns(country);
+
+            var res = await _service.GetSingle("bla");
+
+            A.CallTo(() => _repository.GetByCodeAsync("bla")).MustHaveHappenedOnceExactly();
+        }
+        
+        
+        
     }
 }
