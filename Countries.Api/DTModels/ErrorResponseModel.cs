4aa1aace812a310aaec2c062e1d6d785e904de0a
Borche Ivanov <borche.ivanov@outlook.com>
2020-02-09 18:24:54 +0100

    ErrorRersponseModel added

--- Countries.Api/DTModels/ErrorResponseModel.cs
+++ Countries.Api/DTModels/ErrorResponseModel.cs
@@ -1,7 +1,11 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
 namespace Countries.Api.DTModels
 {
     public class ErrorResponseModel
     {
-        
+        public int? StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
